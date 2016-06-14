9d3ba82a0c20a7ec25b82d4c7ae89b34563e800f
karlomikus <contact@karlomikus.com>
2016-06-14 16:10:03 +0000

    Added welcome homepage

--- config/routes.php
+++ config/routes.php
@@ -8,4 +8,16 @@ $app['action.list-categories'] = function() use ($app) {
     return new App\Category\Action\ListCategoriesAction($app['responder.list-categories'], $app['repository.categories']);
 };
 
-$app->get('/', 'action.list-categories');
+$app->get('/', function () {
+    $msg = [];
+    $msg[] = '<h1>Application API</h1>';
+    $msg[] = '<p>You\'re probably looking for JSON API:</p>';
+    $msg[] = '<ul>';
+    $msg[] = '<li><a href="/api/v1/categories">List all categories</a></li>';
+    $msg[] = '</ul>';
+
+    return implode('', $msg);
+});
+
+/** API */
+$app->get('/api/v1/categories', 'action.list-categories');
