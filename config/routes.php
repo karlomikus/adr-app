03d41f781163d3341d313962a3b86a0b267fac59
karlomikus <contact@karlomikus.com>
2016-06-25 10:11:06 +0000

    Removed a route

--- config/routes.php
+++ config/routes.php
@@ -29,19 +29,6 @@ $app['action.login'] = function() use ($app) {
 /**
  * Routes
  */
-$app->get('/', function () {
-    $msg = [];
-    $msg[] = '<h1>Application API</h1>';
-    $msg[] = '<p>You\'re probably looking for JSON API:</p>';
-    $msg[] = '<ul>';
-    $msg[] = '<li><a href="/api/v1/projects">List all projects</a></li>';
-    $msg[] = '<li><a href="/api/v1/projects/1">Show a specific category</a></li>';
-    $msg[] = '</ul>';
-
-    return implode('', $msg);
-});
-
-/** API */
 $app->get('/api/v1/projects', 'action.list-projects');
 $app->get('/api/v1/projects/{id}', 'action.show-project');
 
